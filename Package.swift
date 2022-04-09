2eac26da62ef4305f2924bc1595de4af59d11d5e
RubenSystems <ruben.t.j@icloud.com>
2022-04-09 10:12:42 +0100

    Fixed Requirements for iOS

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "RSNNSwift",
 	platforms: [
 		.macOS(.v10_15),
-		.iOS(.v10),
+		.iOS(.v13),
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
